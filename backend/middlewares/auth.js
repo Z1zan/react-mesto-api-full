85028d6c801205d3ead18bb9650e0ad9deae9ae5
Ivan Nemirich <bebded@gmail.com>
2021-04-25 14:02:00 +0700

    add & fix: dev режим для .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,12 +3,16 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/authError');
 
 // eslint-disable-next-line no-undef
-// const secret = process.env;
-const secret = process.env.JWT_SECRET | 'SECRET';
+const secret = process.env;
 
 function auth(req, res, next) {
   try {
-    req.user = jwt.verify(req.cookies.jwt, secret);
+    req.user = jwt.verify(
+      req.cookies.jwt,
+      secret.NODE_ENV === 'production'
+        ? secret.JWT_SECRET
+        : 'dev-secret',
+    );
     next();
   } catch (err) {
     next(new AuthError('Нет доступа к данной странице'));
